e7aa31f6198c91c399ba4eb1a67b5b75832b22f4
Devan Singam <dvnsingam@gmail.com>
2015-11-23 01:35:35 +0800

    finally

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,21 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+	validates :email,
+		:uniqueness => true,
+		:format => { :with => /\w+(.)*\w+@\w+(.)*\w+/}
+	validates_numericality_of :age, :greater_than => 5
+	validates :phone,
+		:format => { :with => /\d{3}.+\d{3}.+\d{4}/}
+
+
+ def name
+ 	fullname = "#{first_name} #{last_name}"
+
+ end
+
+ def age
+ 	age = Time.now.year - birthday.year
+ end
+
 end
